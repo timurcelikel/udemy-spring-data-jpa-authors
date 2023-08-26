8d0971f82207ad93f247064f4168a25b5d2041ed
Timur Celikel <timur.celikel@proton.me>
2023-08-26 09:15:01 -0700

    Set up local profile to be used for testing purposes

--- sdjpa-intro/src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ sdjpa-intro/src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -3,8 +3,10 @@ package guru.springframework.sdjpaintro.bootstrap;
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile({ "local", "default" })
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -17,6 +19,8 @@ public class DataInitializer implements CommandLineRunner {
 
 	@Override
 	public void run(final String... args) throws Exception {
+
+		bookRepository.deleteAll();
 
 		Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
 		bookRepository.save(bookDDD);
